9418ac52603b51d4a01cb84d38df215b9b788f19
Ruixian Shan <shanruixian@gmail.com>
2017-04-12 14:07:43 -0500

    set up one-to-many relationship

--- src/main/java/org/launchcode/models/Category.java
+++ src/main/java/org/launchcode/models/Category.java
@@ -1,10 +1,10 @@
 package org.launchcode.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  * Created by RuixianShan on 4/10/17.
@@ -18,6 +18,13 @@ public class Category {
     @NotNull
     @Size(min=3, max=15)
     private String name;
+
+
+    @OneToMany
+    @JoinColumn(name = "category_id")
+    private List<Cheese> cheeses= new ArrayList<>();
+
+
 
     public int getId() {
         return id;
